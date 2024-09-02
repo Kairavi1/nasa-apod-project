dfa4d64eb4ddea5aa3f8d63906beda5b45e9fb2f
Kairavi1 <kairavi1770@gmail.com>
2024-09-02 09:27:36 +0530

    Add files via upload

---
+++ src/components/main.jsx
@@ -0,0 +1,8 @@
+export default function main(props){
+    const{data}=props
+    return(
+        <div className="imgContainer">
+            <img src={data.hdurl} alt={data.title || 'bg-img'} className="bgImage" /> 
+        </div>
+    )
+}
